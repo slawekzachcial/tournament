b0b9ef0c193ee22bbccda6c3d2323bb62898648b
Slawek Zachcial <slawek.zachcial@gmail.com>
2021-03-01 22:21:56 +0100

    Create postgres data directory

--- tools/run-postgres.sh
+++ tools/run-postgres.sh
@@ -4,10 +4,13 @@ if ! docker network inspect postgres-net &>/dev/null; then
     docker network create --driver bridge postgres-net
 fi
 
+POSTGRES_DATA=$(readlink --canonicalize $(dirname ${BASH_SOURCE[0]})/../postgres)
+mkdir -p "${POSTGRES_DATA}"
+
 docker run --rm \
     --name postgres \
     --network postgres-net \
     --publish 5432:5432 \
     --env POSTGRES_PASSWORD=secret \
-    --volume $(readlink --canonicalize $(dirname ${BASH_SOURCE[0]})/../postgres):/var/lib/postgresql/data \
+    --volume "${POSTGRES_DATA}":/var/lib/postgresql/data \
     postgres:alpine
